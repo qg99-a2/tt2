d909066d518c3ae64debebd177e9a87b4f0686bc
qg99-a2 <140741831+qg99-a2@users.noreply.github.com>
2023-08-08 12:29:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+import * as fs from "fs"
+import * as path from "path"
+let Files  = [];
+
+String.prototype.replaceAll = function(search, replacement) {
+  var target = this;
+  return target.split(search).join(replacement);
+};
+
+function ThroughDirectory(Directory) {
+    fs.readdirSync(Directory).forEach(File => {
+        const Absolute = path.join(Directory, File);
+        if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+        else return Files.push(Absolute);
+    });
+}
+
+ThroughDirectory("./node_modules/@puppeteer");
+ThroughDirectory("./node_modules/puppeteer");
+ThroughDirectory("./node_modules/puppeteer-core");
+let js_files = []
+for(let i in Files) if((Files[i] + "").endsWith(".js")){js_files.push(Files[i])}
+for(let i in js_files){
+  let file_content = fs.readFileSync(js_files[i]) + ""
+  file_content = file_content.split("??=").join("=");
+  fs.writeFileSync(js_files[i], file_content)
+}
+
 import puppeteer from 'puppeteer-core';
 import {executablePath} from 'puppeteer';
 
